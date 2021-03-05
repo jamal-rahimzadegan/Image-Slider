53e4dea252b3ac1145081b13452cbfd86f211459
Jamal Rahimzadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-03-05 23:17:29 +0330

    Update SliderBullets.js

--- src/SliderBullets.js
+++ src/SliderBullets.js
@@ -1,20 +1,17 @@
 import {View, Text} from "react-native";
-import styles from "../assets/styles/PostDetails";
-import {DIM_BG, Grey, RedLight, RedMed} from "../assets/styles/GlobalStyle";
-import React from "react";
-import {persianText} from "./functions";
-
 
 export default function SliderBullets(props) {
+    const {activeImg , photos} = props
+    
     return (
         <View style={styles.SliderBulletContainer}>
             {
-                props.Photos.length > 1 ?
-                    props.Photos.map((item, index) => {
+                photos.length > 1 ?
+                    photos.map((item, index) => {
                         return (
                             <View style={{
                                 elevation:1,
-                                backgroundColor: props.ActiveImg == index ? RedMed : 'rgba(220,220,220,.8)',
+                                backgroundColor: activeImg == index ? 'red' : 'rgba(220,220,220,.8)',
                                 height:8,
                                 width:8,
                                 borderRadius:2.7,
