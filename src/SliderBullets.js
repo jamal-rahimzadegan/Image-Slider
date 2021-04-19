c642950e51a74ce76f2248a2e1d057dec16cf8a2
Jamal Rahimzadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-04-19 18:02:04 +0430

    Improve: make the code cleaner

--- src/SliderBullets.js
+++ src/SliderBullets.js
@@ -1,25 +1,31 @@
-import {View, Text} from "react-native";
-
 export default function SliderBullets(props) {
-    const {activeImg , photos} = props
-    
-    return (
-        <View style={styles.SliderBulletContainer}>
-            {
-                photos.length > 1 ?
-                    photos.map((item, index) => {
-                        return (
-                            <View style={{
-                                elevation:1,
-                                backgroundColor: activeImg == index ? 'red' : 'rgba(220,220,220,.8)',
-                                height:8,
-                                width:8,
-                                borderRadius:2.7,
-                                margin: 5,
-                            }}/>
-                        )
-                    }) : null
-            }
-        </View>
-    )
+  const { activeImg, photos } = props;
+
+  const styles = {
+    bulletContainer: {},
+    bulletStyle: {
+      elevation: 1,
+      height: 8,
+      width: 8,
+      borderRadius: 2.7,
+      margin: 5,
+    },
+  };
+
+  return (
+    <View style={styles.bulletContainer}>
+      {photos.length > 1
+        ? photos.map((item, index) => (
+            <View
+              style={[
+                styles.bulletStyle, {
+                  backgroundColor:
+                    activeImg == index ? "red" : "rgba(220,220,220,.8)",
+                },
+              ]}
+            />
+          ))
+        : null}
+    </View>
+  );
 }
